9f582d8b1ba8d788f1682a749ad131077c7c38d5
MohamedDib <49766520+MohamedDib@users.noreply.github.com>
2021-09-23 11:34:31 +0200

    8. Notifications Push - résultat

--- sw.js
+++ sw.js
@@ -93,7 +93,7 @@ self.addEventListener('fetch', (evt) => {
         }
     )*/
 });
-
+/*
 	
 // 7.3 Notifications persistantes (envoyées depuis le service worker)
 self.registration.showNotification("Notification du SW", {
@@ -133,4 +133,18 @@ self.addEventListener("notificationclick", evt => {
   
     // 7.5 Fermer programmatiquement une notification
     evt.notification.close();
+})*/
+
+// 8.1 Intercepter une notification push
+self.addEventListener("push", evt => {
+    console.log("push event", evt);
+    console.log("data envoyée par la push notification :", evt.data.text());
+ 
+    // 8.1 afficher son contenu dans une notification
+    const title = evt.data.text();
+    const objNotification = {
+        body: "ça fonctionne", 
+        icon : "images/icons/icon-72x72.png"
+    };
+    self.registration.showNotification(title, objNotification);
 })
